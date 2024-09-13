2be42268c41c9a813c05309550096d35b90ffa4a
Gabriel Chiarelli Noble <gabrielcnoble@gmail.com>
2024-09-12 23:33:27 -0300

    make disruptive effects other than out of shape be properly disruptive

--- src/overlays/actors/ovl_player_actor/z_player.c
+++ src/overlays/actors/ovl_player_actor/z_player.c
@@ -7095,7 +7095,9 @@ s32 func_808373F8(PlayState* play, Player* this, u16 sfxId) {
         anim = &gPlayerAnim_link_normal_run_jump;
     }
 
-    if(gChaosContext.link.out_of_shape_state == CHAOS_OUT_OF_SHAPE_STATE_NONE)
+    if(gChaosContext.link.out_of_shape_state == CHAOS_OUT_OF_SHAPE_STATE_NONE && 
+       gChaosContext.link.imaginary_friends_state == CHAOS_IMAGINARY_FRIENDS_STATE_NONE &&
+       gChaosContext.link.sneeze_state == CHAOS_SNEEZE_STATE_NONE)
     {
         // Deku hopping
         if (this->transformation == PLAYER_FORM_DEKU) {
@@ -17526,6 +17528,8 @@ s32 func_80850854(PlayState* play, Player* this) {
 
     if ((this->transformation == PLAYER_FORM_DEKU) && (this->remainingHopsCounter != 0) && 
         gChaosContext.link.out_of_shape_state != CHAOS_OUT_OF_SHAPE_STATE_GASPING &&
+        gChaosContext.link.sneeze_state != CHAOS_SNEEZE_STATE_SNEEZE &&
+        gChaosContext.link.imaginary_friends_state != CHAOS_IMAGINARY_FRIENDS_STATE_SCHIZO &&
         (gSaveContext.save.saveInfo.playerData.health != 0) && (sPlayerControlStickMagnitude != 0.0f)) {
         func_808373F8(play, this, 0);
         return true;
@@ -18941,7 +18945,9 @@ void Player_Action_77(Player* this, PlayState* play) {
             Audio_PlaySfx(NA_SE_OC_ABYSS);
 
             if(gChaosContext.link.fierce_deity_state != CHAOS_RANDOM_FIERCE_DEITY_STATE_NONE &&
-                gChaosContext.link.out_of_shape_state != CHAOS_OUT_OF_SHAPE_STATE_NONE)
+                (gChaosContext.link.out_of_shape_state != CHAOS_OUT_OF_SHAPE_STATE_NONE || 
+                 gChaosContext.link.sneeze_state != CHAOS_SNEEZE_STATE_NONE ||
+                 gChaosContext.link.imaginary_friends_state != CHAOS_IMAGINARY_FRIENDS_STATE_NONE))
             {
                 /* player drowned as fierce deity, so deactivate the effect to avoid softlocks */
                 Chaos_DeactivateCode(CHAOS_CODE_RANDOM_FIERCE_DEITY);
@@ -19868,8 +19874,9 @@ Vec3f D_8085D96C = { 30.0f, 50.0f, 0.0f };
 
 // Flying as Deku?
 void Player_Action_94(Player* this, PlayState* play) {
-    u32 out_of_shape = gChaosContext.link.out_of_shape_speed_scale < 0.1f;
-    u32 need_to_sneeze = gChaosContext.link.sneeze_speed_scale < 0.1f;
+    u32 out_of_shape = gChaosContext.link.out_of_shape_state == CHAOS_OUT_OF_SHAPE_STATE_GASPING;
+    u32 need_to_sneeze = gChaosContext.link.sneeze_speed_scale == CHAOS_SNEEZE_STATE_SNEEZE;
+    u32 is_being_kinda_schizo = gChaosContext.link.imaginary_friends_state == CHAOS_IMAGINARY_FRIENDS_STATE_SCHIZO;
 
     gPlayerAction = 94;
 
@@ -19877,7 +19884,7 @@ void Player_Action_94(Player* this, PlayState* play) {
         this->boomerangActor = NULL;
     }
 
-    if(out_of_shape || need_to_sneeze)
+    if(out_of_shape || need_to_sneeze || is_being_kinda_schizo)
     {
         sPlayerControlInput->press.button |= BTN_A;
     }
@@ -19923,7 +19930,7 @@ void Player_Action_94(Player* this, PlayState* play) {
         this->actor.terminalVelocity = -10.0f;
         this->actor.gravity = -0.5f;
         Player_ResetCylinder(this);
-    } else if (out_of_shape || need_to_sneeze || CHECK_BTN_ALL(sPlayerControlInput->press.button, BTN_A)) {
+    } else if (out_of_shape || need_to_sneeze || is_being_kinda_schizo || CHECK_BTN_ALL(sPlayerControlInput->press.button, BTN_A)) {
         func_808355D8(play, this, &gPlayerAnim_pn_kakkufinish);
     } else {
         s16 temp_a0;
